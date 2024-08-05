a333dc57d527b51c01d9b36f282c808d5da8b3e9
DuckBat <32737124+duckbat@users.noreply.github.com>
2024-08-05 21:00:46 +0300

    changed server, fixed dev devependence

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const config = require('config');
 
 
 const app = express ();
-const PORT = config.get('serverPort') || 5000;
+const PORT = config.get('serverPort') || 3000;
 
 const start = async() => {
     try {
